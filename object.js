703c9beacd8d31995d997a5116d0cb619dde1fd0
topshager <rem28777@gmail.com>
2025-01-12 22:00:21 +0200

    Create object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+const myObject = {
+  property: 'Value',
+  otherProperty: 77,
+  "obnoxiose property": function(){
+
+  }
+}
+//dot notation
+myObject.property;
+
+//bracket notation
+myObject["obnoxiose property"];
